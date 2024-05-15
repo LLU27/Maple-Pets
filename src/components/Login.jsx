81e3021d028bb0ee8542d3906d8bcc602aeab13f
Le Lu <le@Les-MacBook-Pro.fios-router.home>
2024-05-15 01:36:19 -0400

    added styling to forms

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import signupBG from '../assets/Images/signup.png'
-import { Link } from 'react-router-dom'
 
 const Login = ({ toggleView }) => {
   const [email, setEmail] = useState('')
@@ -13,35 +12,51 @@ const Login = ({ toggleView }) => {
       alert('Please fill in both fields.')
       return
     }
+
+    // Implement login logic here
     console.log('email:', email, 'password:', password)
   }
 
   return (
     <div className='bg-cover bg-center h-screen' style={{ backgroundImage: `url(${signupBG})` }}>
-      <h2>Login</h2>
+      <div className='flex justify-center items-center h-full'>
+        <form onSubmit={handleFormSubmit} className='border border-blue-500 p-8 flex flex-col bg-white rounded-lg shadow-lg'>
+          <h1 className='text-center text-3xl mb-6 font-maplestory'>Login</h1>
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>Email:</label>
+            <input
+              type='email'
+              name='email'
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight '
+            />
+          </div>
 
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label>Email:</label>
-          <input type='email' name='email' value={email} onChange={e => setEmail(e.target.value)} />
-        </div>
+          <div className='mb-6'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>Password:</label>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight  '
+            />
+          </div>
 
-        <div>
-          <label>Password:</label>
-          <input type='password' name='password' value={password} onChange={e => setPassword(e.target.value)} />
-        </div>
-        <div className='flex'>
-          <button
-            type='submit'
-            className='bg-[#f68d12] border-2 px-10 py-2 font-bold text-[17px] text-white rounded-[5rem] hover:bg-[#e07a0e] hover:scale-105 transform transition-transform duration-300 ease-in-out'
-          >
-            Log In
-          </button>
-          <button type='button' onClick={toggleView} className='ml-4 text-[#f68d12] underline'>
-            Don't have an account? Sign up now!
-          </button>
-        </div>
-      </form>
+          <div className='flex items-center justify-between'>
+            <button
+              type='submit'
+              className='bg-[#f68d12] border-2 px-10 py-2 font-bold text-[17px] text-white rounded-[5rem] hover:bg-[#e07a0e] hover:scale-105 transform transition-transform duration-300 ease-in-out font-maplestory'
+            >
+              Log In
+            </button>
+            <button type='button' onClick={toggleView} className='ml-4 text-[#f68d12] underline font-maplestory'>
+              Don't have an account? Sign up now!
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -30,39 +30,64 @@ const SignUp = ({ toggleView }) => {
 
   return (
     <div className='bg-cover bg-center h-screen' style={{ backgroundImage: `url(${signupBG})` }}>
-      <h2>Sign Up</h2>
+      <div className='flex justify-center items-center h-full'>
+        <form onSubmit={handleFormSubmit} className='border border-blue-500 p-8 flex flex-col bg-white rounded-lg shadow-lg'>
+          <h1 className='text-center text-3xl mb-6 font-maplestory'>Sign Up</h1>
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>First Name:</label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight '
+              type='text'
+              name='firstName'
+              value={userInfo.firstName}
+              onChange={handleChange}
+            />
+          </div>
 
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label>First Name:</label>
-          <input type='text' name='firstName' value={userInfo.firstName} onChange={handleChange} />
-        </div>
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>Last Name:</label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight font-maplestory'
+              type='text'
+              name='lastName'
+              value={userInfo.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>Email:</label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight '
+              type='email'
+              name='email'
+              value={userInfo.email}
+              onChange={handleChange}
+            />
+          </div>
 
-        <div>
-          <label>Last Name:</label>
-          <input type='text' name='lastName' value={userInfo.lastName} onChange={handleChange} />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input type='email' name='email' value={userInfo.email} onChange={handleChange} />
-        </div>
-
-        <div>
-          <label>Password:</label>
-          <input type='password' name='password' value={userInfo.password} onChange={handleChange} />
-        </div>
-        <div className='flex'>
-          <button
-            type='submit'
-            className='bg-[#f68d12] border-2 px-10 py-2 font-bold text-[17px] text-white rounded-[5rem] hover:bg-[#e07a0e] hover:scale-105 transform transition-transform duration-300 ease-in-out'
-          >
-            Sign Up
-          </button>
-          <button type='button' onClick={toggleView} className='ml-4 text-[#f68d12] underline'>
-            Have an account already? Login Now
-          </button>
-        </div>
-      </form>
+          <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2 font-maplestory'>Password:</label>
+            <input
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight '
+              type='password'
+              name='password'
+              value={userInfo.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='flex'>
+            <button
+              type='submit'
+              className='bg-[#f68d12] border-2 px-10 py-2 font-bold text-[17px] text-white rounded-[5rem] hover:bg-[#e07a0e] hover:scale-105 transform transition-transform duration-300 ease-in-out font-maplestory'
+            >
+              Sign Up
+            </button>
+            <button type='button' onClick={toggleView} className='ml-4 text-[#f68d12] underline font-maplestory'>
+              Have an account already? Login Now
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
