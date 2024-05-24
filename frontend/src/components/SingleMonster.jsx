a21ec9712e50bdfa275c1480c574b54ad5c5e80b
Le Lu <le@Les-MacBook-Pro.fios-router.home>
2024-05-24 04:05:34 -0400

    added styling to music player, single monster page done for now

--- frontend/src/components/SingleMonster.jsx
+++ frontend/src/components/SingleMonster.jsx
@@ -8,18 +8,19 @@ import { TiArrowLeftThick } from 'react-icons/ti'
 const MonsterDetail = () => {
   let { id } = useParams()
   const navigate = useNavigate()
-
-  const [monster, setMonster] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
-  const [location, setLocation] = useState(null)
-  const [bgm, setBgm] = useState(null)
+  const [monsterData, setMonsterData] = useState(null)
 
   const fetchLocationDetails = async locationId => {
     try {
       const { data } = await axios.get(`https://maplestory.io/api/GMS/250/map/${locationId}`)
-      setBgm(`https://maplestory.io/api/GMS/250/map/${locationId}/bgm`)
-      return { streetName: data.streetName, name: data.name }
+      return {
+        streetName: data.streetName,
+        name: data.name,
+        bgm: data.backgroundMusic,
+        bgmUrl: `https://maplestory.io/api/GMS/250/map/${locationId}/bgm`,
+      }
     } catch (error) {
       console.error('Error fetching location details:', error)
       setError('Failed to fetch location details')
@@ -30,10 +31,11 @@ const MonsterDetail = () => {
   const fetchSingleMonster = async id => {
     try {
       const { data } = await axios.get(`https://maplestory.io/api/GMS/250/mob/${id}`)
-      setMonster(data)
       if (data.foundAt && data.foundAt.length > 0) {
         const locationDetails = await fetchLocationDetails(data.foundAt[0])
-        setLocation(locationDetails)
+        setMonsterData({ monster: data, location: locationDetails })
+      } else {
+        setMonsterData({ monster: data, location: null })
       }
     } catch (error) {
       console.error('Error fetching mob detail:', error)
@@ -55,26 +57,19 @@ const MonsterDetail = () => {
     return <div>{error}</div>
   }
 
-  if (!monster) {
+  if (!monsterData) {
     return <div>No monster data available</div>
   }
 
   return (
     <div>
       <Nav />
-      <div className='container m-auto my-4 '>
-        <button onClick={() => navigate(-1)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-4'>
+      <div className='container m-auto p-2 '>
+        <button onClick={() => navigate(-1)} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded  m-4'>
           <TiArrowLeftThick />
         </button>
-        <SingleMonsterCard monster={monster} location={location} bgm={bgm} />
-        {location && (
-          <div className='my-4'>
-            <h2>Found at Location:</h2>
-            <p>
-              {location.streetName} - {location.name}
-            </p>
-          </div>
-        )}
+
+        <SingleMonsterCard monsterData={monsterData} />
       </div>
     </div>
   )
--- frontend/src/components/SingleMonsterCard.jsx
+++ frontend/src/components/SingleMonsterCard.jsx
@@ -1,27 +1,29 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-const SingleMonsterCard = ({ monster, location, bgm }) => {
+const SingleMonsterCard = ({ monsterData }) => {
+  const { monster, location, bgm, bgmUrl } = monsterData
+  console.log(monsterData)
   const [isMoving, setIsMoving] = useState(false)
   const audioRef = useRef(null)
 
   useEffect(() => {
-    if (audioRef.current) {
+    if (audioRef.current && bgm) {
       audioRef.current.play()
     }
-  }, [])
+  }, [bgm])
 
   const toggleAnimation = () => {
     setIsMoving(!isMoving)
   }
 
   return (
-    <div className='shadow-lg rounded-lg bg-[#f68d12] p-1 md:p-2 w-[90%] md:w-[50%] lg:w-[25%] m-auto text-center text-white'>
-      <div className='flex justify-between px-2'>
-        <h3 className='text-lg font-semibold mb-2 text-white'>{monster.name}</h3>
-        <p className='text-white'>Level {monster.meta.level}</p>
+    <div className='shadow-lg rounded-lg bg-[#f68d12] p-4 w-[90%] md:w-[50%] lg:w-[30%] m-auto text-center text-white'>
+      <div className='flex justify-between items-center mb-4 px-4'>
+        <h3 className='text-lg font-semibold'>{monster.name}</h3>
+        <p className='text-sm'>Level {monster.meta.level}</p>
       </div>
 
-      <div className='bg-white p-2 h-48 flex justify-center items-center overflow-hidden rounded-md'>
+      <div className='bg-white p-4 h-48 flex justify-center items-center overflow-hidden rounded-md mb-4'>
         <img
           src={`https://maplestory.io/api/GMS/250/mob/${monster.id}/render/${isMoving ? 'move' : 'stand'}`}
           alt={monster.name}
@@ -29,22 +31,26 @@ const SingleMonsterCard = ({ monster, location, bgm }) => {
         />
       </div>
 
-      <div className='p-4'>
-        <p className='text-sm'>Health : {monster.meta.maxHP}</p>
-        <p className='text-sm'>Damage : {monster.meta.physicalDamage}</p>
-        <p className='text-sm'>Experience + {monster.meta.exp}</p>
-        <p className='text-sm'>Summon Type : {monster.meta.summonType}</p>
-        {location && <p className='text-sm'>Found at : {`${location.name} - ${location.streetName}`}</p>}
-        {bgm && (
-          <div className='my-4'>
-            <h2>Background Music:</h2>
-            <audio ref={audioRef} controls src={bgm} loop autoPlay>
-              Your browser does not support the audio element.
-            </audio>
-          </div>
+      <div className='px-4'>
+        <p className='text-sm mb-2'>Health: {monster.meta.maxHP}</p>
+        <p className='text-sm mb-2'>Damage: {monster.meta.physicalDamage}</p>
+        <p className='text-sm mb-2'>Experience: {monster.meta.exp}</p>
+        <p className='text-sm mb-2'>Summon Type: {monster.meta.summonType}</p>
+        {location && (
+          <p className='text-sm mb-2'>
+            Found at: {location.name} - {location.streetName}
+          </p>
         )}
-        <button className='mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded' onClick={toggleAnimation}>
-          {isMoving ? 'Stand ' : 'Move '}
+
+        <div className='my-4'>
+          <p className='text-sm mb-2'>Background Music: {location.bgm}</p>
+          <audio ref={audioRef} controls src={location.bgmUrl} loop autoPlay className='w-full'>
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+
+        <button className='mt-4 px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-semibold rounded w-full' onClick={toggleAnimation}>
+          {isMoving ? 'Stand' : 'Move Animation'}
         </button>
       </div>
     </div>
